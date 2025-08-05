9ef051d7a8d390dfbe66de3fd2f4e9d94a568f2b
Catman06 <zippercat06@gmail.com>
2025-08-05 14:07:31 -0500

    Improve the way charts are displayed

--- src/components/ChartDisplay.vue
+++ src/components/ChartDisplay.vue
@@ -10,19 +10,35 @@ import { computed } from 'vue';
 
 <template>
 	<div id="info" v-if="chart">
-		<p class="label">Starting Location</p>
-		<p>{{ chart['start'] }}</p>
-		<p class="label">Ending Location</p>
-		<p>{{ chart['endpoint'] }}</p>
-		<p class="label">Route Stability</p>
-		<p>{{ chart['stability'] }}</p>
-		<p class="label">Travel Duration</p>
-		<p>{{ chart['duration'] }} days</p>
-		<p class="label">Chart Quality</p>
-		<p>{{ chart['quality'] }}</p>
-		<p class="label">Chart Maker</p>
-		<p>{{ chart['maker'] }}</p>
-		<p class="label">Astronomican</p>
+		<div>
+			<div>
+				<p class="label">Starting Location</p>
+				<p>{{ chart['start'] }}</p>
+			</div>
+			<div>
+				<p class="label">Ending Location</p>
+				<p>{{ chart['endpoint'] }}</p>
+			</div>
+			<div>
+				<p class="label">Route Stability</p>
+				<p>{{ chart['stability'] }}</p>
+			</div>
+		</div>
+		<div>
+			<div>
+				<p class="label">Travel Duration</p>
+				<p>{{ chart['duration'] }} days</p>
+			</div>
+			<div>	
+				<p class="label">Chart Quality</p>
+				<p>{{ chart['quality'] }}</p>
+			</div>
+			<div>
+				<p class="label">Chart Maker</p>
+				<p>{{ chart['maker'] }}</p>
+			</div>
+		</div>
+			<p class="label">Astronomican</p>
 		<p>{{ chart['astronomican'] }}</p>
 		<p class="label" v-if="chart['rules']">Special Rules</p>
 		<p >{{ chart['rules'] }}</p>
@@ -30,5 +46,22 @@ import { computed } from 'vue';
 </template>
 
 <style lang="css" scoped>
+	#info {
+		color: rgba(0, 0, 0, 0.729);
+		background-color: #a5a078;
+		padding: 10px;
+		margin: 5px;
 
+		&>div {
+			display: flex;
+			justify-content: space-around;
+
+			&>div {
+				display: block;
+			}
+		}
+	}
+	.label {
+		font-weight: bolder;
+	}
 </style>
