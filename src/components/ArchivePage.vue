f7b993146bb0a9f548a75e3e4694717314b9d02d
Catman06 <zippercat06@gmail.com>
2025-08-09 06:44:27 -0500

    Changed PDFs to open in a new tab

--- src/components/ArchivePage.vue
+++ src/components/ArchivePage.vue
@@ -14,34 +14,34 @@
 			<a href="Downloads/Heraldry Valentine.svg" download>
 				VALENTINE HERALDIC ACHEIVEMENT (SVG)
 			</a>
-			 | USES FONTS "<a href="https://www.dafont.com/cloister-black.font">CLOISTER BLACK</a>" & "<a href="https://fonts.google.com/specimen/IM+Fell+English+SC?query=IM+fell">IM FELL ENGLISH SC</a>"
+			 | USES FONTS "<a href="https://www.dafont.com/cloister-black.font" target="_blank">CLOISTER BLACK</a>" & "<a href="https://fonts.google.com/specimen/IM+Fell+English+SC" target="_blank">IM FELL ENGLISH SC</a>"
 		</p>
 		<a href="Downloads/Heraldry Valentine.png" download>
 			<p>VALENTINE HERALDIC ACHEIVEMENT (PNG)</p>
 		</a>
 		<p>
 			<a href="Downloads/Koronus Expanse Printable.svg" download>
-				KORONUS EXPANSE MAP
+				KORONUS EXPANSE MAP (SVG)
 			</a>
-			 | USES FONTS "<a href="https://fonts.google.com/specimen/IM+Fell+English+SC?query=IM+fell">IM FELL ENGLISH SC</a>" & "<a href="Downloads/REQUIEM-Inquisitor.otf" download>REQUIEM-INQUISITOR</a>"
+			 | USES FONTS "<a href="https://fonts.google.com/specimen/IM+Fell+English+SC" target="_blank">IM FELL ENGLISH SC</a>" & "<a href="Downloads/REQUIEM-Inquisitor.otf" download>REQUIEM-INQUISITOR</a>"
 		</p>
 		<a href="Downloads/Valentine Coat of Arms Logo.svg" download>
-			<p>VALENTINE LOGO</p>
+			<p>VALENTINE LOGO (SVG)</p>
 		</a>
 		<a href="Downloads/Downloads/Valentine Coat of Arms Logo Greyscale.svg" download>
-			<p>VALENTINE LOGO GREYSCALE</p>
+			<p>VALENTINE LOGO GREYSCALE (SVG)</p>
 		</a>
 		<p>++ PRINTABLES</p>
 		<p>
-			<a href="Downloads/roguetrader_charactersheet_v3_Valentine.pdf" download>CHARACTER SHEET</a>
-			 | BASED ON <a href="https://georgellovett.wordpress.com/portfolio/rogue-trader-character-sheet-redesign/">THIS</a> SHEET
+			<a href="Downloads/roguetrader_charactersheet_v3_Valentine.pdf" target="_blank">CHARACTER SHEET (PDF)</a>
+			 | BASED ON <a href="https://georgellovett.wordpress.com/portfolio/rogue-trader-character-sheet-redesign/" target="_blank">THIS</a> SHEET
 		</p>
-		<a href="Downloads/NPC Sheets.pdf" download>NPC SHEETS</a>
+		<a href="Downloads/NPC Sheets.pdf" target="_blank">NPC SHEETS (PDF)</a>
 		<p>
-			<a href="Downloads/Ship Sheet.pdf" download>SHIP SHEET</a>
+			<a href="Downloads/Ship Sheet.pdf" target="_blank">SHIP SHEET (PDF)</a>
 			 | BASED ON THE ONE IN THE CORE RULEBOOK
 		</p>
-		<a href="Downloads/Vehicle Sheet.pdf" download>VEHICLE SHEET</a>
+		<a href="Downloads/Vehicle Sheet.pdf" target="_blank">VEHICLE SHEET (PDF)</a>
 	</div>
 </template>
 
